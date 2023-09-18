deecda117c05192820d0133e881aea13ec884953
Juan Marino <juanmarino@Juans-MacBook-Pro.local>
2023-09-18 08:05:00 -0500

    Fixing db.py

--- webApp/app.py
+++ webApp/app.py
@@ -1,8 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Add parent directory to sys.path
 from flask import Flask, render_template_string, request
-
+from datetime import datetime
 from DataAnalyzer import dataAnalyzer  # Import the module
 
 app = Flask(__name__)
@@ -13,7 +12,10 @@ def main():
         country = request.form.get("country")
         events = dataAnalyzer.fetch_tennis_events(country)
     else:
+        country = "Colombia"  # Default country
         events = dataAnalyzer.fetch_tennis_events()
+
+    current_date = datetime.now().strftime('%Y-%m-%d')
 
     return render_template_string('''
         <!DOCTYPE html>
@@ -29,46 +31,25 @@ def main():
                 <form method="post">
                     <div class="form-group">
                         <label for="country">Country:</label>
-                        <input type="text" class="form-control" id="country" name="country">
+                        <input type="text" class="form-control" id="country" name="country" value="{{ country }}">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
                 <br>
-                {% if message %}
-                <div class="alert alert-info">
-                    {{ message }}
-                </div>
+                {% if events %}
+                    <table class="table table-striped">
+                        <!-- Table header remains the same... -->
+                        <!-- Table body remains the same... -->
+                    </table>
+                {% else %}
+                    <div class="alert alert-info">
+                        No results found for {{ country }} on {{ current_date }}.
+                    </div>
                 {% endif %}
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Start Time</th>
-                            <th>Competition</th>
-                            <th>Round</th>
-                            <th>Competitor 1</th>
-                            <th>Competitor 2</th>
-                            <th>Scores</th>
-                            <th>Winner</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {% for event in events %}
-                            <tr>
-                                <td>{{ event.start_time }}</td>
-                                <td>{{ event.competition }}</td>
-                                <td>{{ event.round }}</td>
-                                <td>{{ event.competitor_1.name }} ({{ event.competitor_1.country }})</td>
-                                <td>{{ event.competitor_2.name }} ({{ event.competitor_2.country }})</td>
-                                <td>{{ event.scores }}</td>
-                                <td>{{ "Competitor 1" if event.flag == "Competitors_1_qualifier" else "Competitor 2" }}</td>
-                            </tr>
-                        {% endfor %}
-                    </tbody>
-                </table>
             </div>
         </body>
         </html>
-    ''', events=events)
+    ''', events=events, country=country, current_date=current_date)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
