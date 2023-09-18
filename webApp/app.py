b28a0df2131d72512fdb3ee0274903b03bd78df4
Juan Marino <juanmarino@Juans-MacBook-Pro.local>
2023-09-18 07:05:40 -0500

    Fixing db.py

--- webApp/app.py
+++ webApp/app.py
@@ -1,79 +1,77 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Add parent directory to sys.path
-from flask import Flask, render_template_string,request
+import requests
+from flask import Flask, render_template_string, request
 
-# Assuming the data analyzer is in the same directory or accessible in the sys.path
-from DataAnalyzer import dataAnalyzer  # Import the module
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from DataAnalyzer import dataAnalyzer
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    events = dataAnalyzer.fetch_colombian_events()
-    return render_template_string('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Colombian Events</title>
-            <!-- Link to Bootstrap CSS for styling -->
-            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        </head>
-        <body>
-            <div class="container">
-                <h2>Colombian Events</h2>
-                 <form action="/fetch_data" method="POST">
-                    <label for="country">Country:</label>
-                    <input type="text" id="country" name="country" required>
-                    
-                    <label for="start_date">Start Date:</label>
-                    <input type="date" id="start_date" name="start_date" required>
-                    
-                    <label for="end_date">End Date:</label>
-                    <input type="date" id="end_date" name="end_date" required>
-                    
-                    <input type="submit" value="Fetch Data">
-                </form>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Start Time</th>
-                            <th>Competition</th>
-                            <th>Round</th>
-                            <th>Competitor 1</th>
-                            <th>Competitor 2</th>
-                            <th>Scores</th>
-                            <th>Winner</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {% for event in events %}
-                            <tr>
-                                <td>{{ event.start_time }}</td>
-                                <td>{{ event.competition }}</td>
-                                <td>{{ event.round }}</td>
-                                <td>{{ event.competitor_1.name }} ({{ event.competitor_1.country }})</td>
-                                <td>{{ event.competitor_2.name }} ({{ event.competitor_2.country }})</td>
-                                <td>{{ event.scores }}</td>
-                                <td>{{ "Competitor 1" if event.flag == "Competitors_1_qualifier" else "Competitor 2" }}</td>
-                            </tr>
-                        {% endfor %}
-                    </tbody>
-                </table>
+TEMPLATE_STRING = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Colombian Events</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h2>Colombian Events</h2>
+        
+        <form method="post">
+            <div class="form-group">
+                <label for="country">Country:</label>
+                <input type="text" class="form-control" id="country" name="country">
             </div>
-        </body>
-        </html>
-    ''', events=events)
-@app.route("/fetch_data", methods=["POST"])
-def fetch_data():
-    country = request.form.get("country")
-    start_date = request.form.get("start_date")
-    end_date = request.form.get("end_date")
+            <div class="form-group">
+                <label for="start_date">Start Date:</label>
+                <input type="date" class="form-control" id="start_date" name="start_date">
+            </div>
+            <div class="form-group">
+                <label for="end_date">End Date:</label>
+                <input type="date" class="form-control" id="end_date" name="end_date">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <br>
+        {% if message %}
+        <div class="alert alert-info">
+            {{ message }}
+        </div>
+        {% endif %}
+        
+        <!-- Displaying the events table -->
+        <table class="table table-striped">
+            <!-- ... [table headers and rows here] -->
+        </table>
+    </div>
+</body>
+</html>
+'''
 
-    # Start the data collection process (pseudo code)
-    # e.g. start_data_collection(country, start_date, end_date)
+@app.route("/", methods=["GET", "POST"])
+def main():
+    message = None
+    if request.method == "POST":
+        country = request.form["country"]
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
 
-    return f"Data collection started for {country} from {start_date} to {end_date}. Please wait while we process your request."
+        response = requests.post("http://api_collector_host:5001/collect_data", json={
+            "country": country,
+            "start_date": start_date,
+            "end_date": end_date
+        })
+
+        api_response = response.json()
+        if api_response.get("status") == "success":
+            message = api_response.get("message")
+        else:
+            message = "Failed to start data collection!"
+
+    events = dataAnalyzer.fetch_colombian_events()
+    return render_template_string(TEMPLATE_STRING, events=events, message=message)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
