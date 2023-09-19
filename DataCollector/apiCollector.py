8bc059f878c97dcd61ac2b23932d9cfaa4049592
Juan Marino <juanmarino@Juans-MacBook-Pro.local>
2023-09-19 06:50:04 -0500

    Added another method to datAnalyzer to summarize wins by country

--- DataCollector/apiCollector.py
+++ DataCollector/apiCollector.py
@@ -29,6 +29,7 @@ def get_custom_event_id(event):
     return competition_id + season_id + competitor_1_id + competitor_2_id
 def structure_data(event):
     competitors = event["sport_event"]["competitors"]
+
     # Check if "period_scores" exists
     scores = event["sport_event_status"].get("period_scores", [])
 
@@ -65,38 +66,27 @@ def structure_data(event):
 
 
 
-def main():
-    set_process_status("datacollector", True)
+set_process_status("datacollector", True)
+# Calculate the date for today - 1
+yesterday = datetime.now() - timedelta(1)
+formatted_yesterday = yesterday.strftime('%Y-%m-%d')
+print(formatted_yesterday)
 
-    # Calculate the date for yesterday
-    yesterday = datetime.now() - timedelta(1)
-    formatted_yesterday = yesterday.strftime('%Y-%m-%d')
-    print(formatted_yesterday)
+# Fetching the data from the API
+url = f"http://api.sportradar.us/tennis/trial/v3/en/schedules/{formatted_yesterday}/summaries.json?api_key=uqmpq6cdah4d25ww4wep2znp"
+response = requests.get(url)
+data = response.json()
 
-    # Fetch data from the API
-    url = f"http://api.sportradar.us/tennis/trial/v3/en/schedules/{formatted_yesterday}/summaries.json?api_key=uqmpq6cdah4d25ww4wep2znp"
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code.
-        data = response.json()
+# Filter and structure the data based on the given requirements
+# Apply our structure function
+structured_data = [structure_data(event) for event in data["summaries"]]
 
-        # Filter and structure the data
-        structured_data = [structure_data(event) for event in data["summaries"]]
+# Insert the data into the database
+for event in structured_data:
+    if not check_event_id_exists(event["event_id"]):  # check for each event
+        insert_event(event)
+        print('data inserted')
+print('Data insertion done')
+set_process_status("datacollector", False)
 
-        # Insert the data into the database
-        for event in structured_data:
-            if not check_event_id_exists(event["event_id"]):  # Check for each event
-                insert_event(event)
-                print('Data inserted')
-
-        print('Data insertion done')
-
-    except requests.RequestException as e:
-        print(f"Error fetching data from the API: {e}")
-
-    finally:
-        set_process_status("datacollector", False)
-
-if __name__ == "__main__":
-    main()
