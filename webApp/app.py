19ca765b2b81f9eced93193b6c0a123ef9a1e106
Juan Marino <juasmarino@gmail.com>
2023-09-26 07:40:01 -0500

    added prometheus

--- webApp/app.py
+++ webApp/app.py
@@ -1,10 +1,10 @@
 import os
 import sys
-from flask import Flask, render_template_string, request, Response
-from datetime import datetime, timedelta
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Add parent directory to sys.path
+from flask import Flask, render_template_string, request,Response
+from datetime import datetime,timedelta
 from prometheus_client import start_http_server, Summary, Counter, generate_latest, CONTENT_TYPE_LATEST
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Add parent directory to sys.path
 from DataAnalyzer import dataAnalyzer  # Import the module
 
 app = Flask(__name__)
@@ -34,7 +34,85 @@ def main():
     stats = dataAnalyzer.country_statistics()
 
     return render_template_string('''
-        <!-- your previous HTML template -->
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Tennis Events</title>
+            <!-- Link to Bootstrap CSS for styling -->
+            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        </head>
+        <body>
+            <div class="container">
+                <h2>Tennis Events Results on {{ yesterday }}</h2>
+                <form method="post">
+                    <div class="form-group">
+                        <label for="country">Country:</label>
+                        <input type="text" class="form-control" id="country" name="country">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                <br>
+                {% if message %}
+                <div class="alert alert-info">
+                    {{ message }}
+                </div>
+                {% endif %}
+                {% if events %}
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Start Time</th>
+                                <th>Competition</th>
+                                <th>Round</th>
+                                <th>Competitor 1</th>
+                                <th>Competitor 2</th>
+                                <th>Scores</th>
+                                <th>Winner</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {% for event in events %}
+                                <tr>
+                                    <td>{{ event.start_time }}</td>
+                                    <td>{{ event.competition }}</td>
+                                    <td>{{ event.round }}</td>
+                                    <td>{{ event.competitor_1.name }} ({{ event.competitor_1.country }})</td>
+                                    <td>{{ event.competitor_2.name }} ({{ event.competitor_2.country }})</td>
+                                    <td>{{ event.scores }}</td>
+                                    <td>{{ "Competitor 1" if event.flag == "Competitors_1_qualifier" else "Competitor 2" }}</td>
+                                </tr>
+                            {% endfor %}
+                        </tbody>
+                    </table>
+                {% else %}
+                    <div class="alert alert-info">
+                        No results found for tennis players from {{ country }} on {{ yesterday }}.
+                    </div>
+                {% endif %}
+                <div class="panel panel-default">
+                    <div class="panel-heading">Results by Country</div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Country</th>
+                                <th>Total Players</th>
+                                <th>Players Who Won</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {% for country, data in stats.items() %}
+                            <tr>
+                                <td>{{ country }}</td>
+                                <td>{{ data.players }}</td>
+                                <td>{{ data.winners }}</td>
+                            </tr>
+                        {% endfor %}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </body>
+        </html>
     ''', events=events, country=country, yesterday=formatted_yesterday, stats=stats)
 
 if __name__ == "__main__":
